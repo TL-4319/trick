505b48a1cab0b8e940641e813de27131b3931482
Tuan Luong <tdluong@crimson.ua.edu>
2025-03-05 13:16:46 -0600

    add GUI forwarding from container. Tested plotting function

--- DockerPlayGround/start_container.sh
+++ DockerPlayGround/start_container.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run --rm -it -v ./workspace:/workspace trick:19.7.3
+docker run --rm -it --privileged \
+    -v ./workspace:/workspace \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+    -e DISPLAY=$DISPLAY \
+    -p 18570:18570/udp trick:19.7.3
